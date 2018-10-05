b213f31136647456807ea17890bc7722af4cd8f4
Dimitar Valkanov <dimitar.valkanov7@gmail.com>
2018-10-05 10:00:18 +0300

    Add tests for title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Home | RoR"
   end
 
   def help
+    @title = "Help | RoR"
   end
 
   def about
+    @title = "About | RoR"
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,4 +15,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+
+  test "should have home title" do
+    get static_pages_home_url
+    assert_select "title", "Home | RoR"
+  end
+
+  test "should have help title" do
+    get static_pages_help_url
+    assert_select "title", "Help | RoR"
+  end
+
+  test "should have about title" do
+    get static_pages_about_url
+    assert_select "title", "About | RoR"
+  end
 end
